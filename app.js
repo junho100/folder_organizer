836f87a4b69b0949560a11fa6e64721ccd53c44c
JunHoBaek <bemodesty306@naver.com>
2021-10-12 23:40:14 +0900

    use "rename"

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("path");
 const PARENTROOT = "./Pictures";
 const TARGETROOT = path.join(PARENTROOT, process.argv[2]);
-let files = [];
 
 if (!fs.existsSync(path.join(TARGETROOT, "video"))) {
   fs.mkdir(path.join(TARGETROOT, "video"), (err) => {
@@ -29,27 +28,33 @@ if (!fs.existsSync(path.join(TARGETROOT, "duplicated"))) {
 const moveFile = (fileName) => {
   if (path.extname(fileName) === ".mp4" || path.extname(fileName) === ".mov") {
     const targetPath = path.join(TARGETROOT, "video");
-    const input = fs.createReadStream(path.join(TARGETROOT, fileName));
-    const output = fs.createWriteStream(path.join(targetPath, fileName));
-    input.pipe(output);
-    fs.rm(path.join(TARGETROOT, fileName));
+    fs.promises
+      .rename(path.join(TARGETROOT, fileName), path.join(targetPath, fileName))
+      .catch((err) => {
+        console.log(err);
+      });
     return;
   }
   if (path.extname(fileName) === ".png" || path.extname(fileName) === ".aae") {
     const targetPath = path.join(TARGETROOT, "captured");
-    const input = fs.createReadStream(path.join(TARGETROOT, fileName));
-    const output = fs.createWriteStream(path.join(targetPath, fileName));
-    input.pipe(output);
-    fs.rm(path.join(TARGETROOT, fileName));
+    fs.promises
+      .rename(path.join(TARGETROOT, fileName), path.join(targetPath, fileName))
+      .catch((err) => {
+        console.log(err);
+      });
     return;
   }
   if (path.extname(fileName) === ".jpg") {
     if (!isNaN(fileName[4])) {
       const targetPath = path.join(TARGETROOT, "duplicated");
-      const input = fs.createReadStream(path.join(TARGETROOT, fileName));
-      const output = fs.createWriteStream(path.join(targetPath, fileName));
-      input.pipe(output);
-      fs.rm(path.join(TARGETROOT, fileName));
+      fs.promises
+        .rename(
+          path.join(TARGETROOT, fileName),
+          path.join(targetPath, fileName)
+        )
+        .catch((err) => {
+          console.log(err);
+        });
       return;
     }
   }
